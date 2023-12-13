5eab64357419c3cc0cd60fecc11b3c9a9f216c68
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-13 13:18:23 +0530

    Update the relation between courses and section

--- master/models.py
+++ master/models.py
@@ -1,6 +1,6 @@
 # master/models.py
-
 from django.db import models
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -8,11 +8,13 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Level(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Requirements(models.Model):
     description = models.TextField()
@@ -20,11 +22,13 @@ class Requirements(models.Model):
     def __str__(self):
         return self.description[:50]  # Displaying the first 50 characters
 
+
 class Outcomes(models.Model):
     description = models.TextField()
 
     def __str__(self):
         return self.description[:50]  # Displaying the first 50 characters
+
 
 class Language(models.Model):
     name = models.CharField(max_length=100)
@@ -32,11 +36,13 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+
 class CourseOverview(models.Model):
     overview = models.TextField()
 
     def __str__(self):
         return self.overview[:50]  # Displaying the first 50 characters
+
 
 class SEOMetakeywords(models.Model):
     keywords = models.TextField()
@@ -44,11 +50,13 @@ class SEOMetakeywords(models.Model):
     def __str__(self):
         return self.keywords[:50]  # Displaying the first 50 characters
 
+
 class PackageType(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Country(models.Model):
     name = models.CharField(max_length=200)
@@ -56,32 +64,34 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+
 class State(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-class City(models.Model):
 
+class City(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Section(models.Model):
-
     name = models.CharField(max_length=200)
+    course = models.ForeignKey("Courses.Course", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
 
-class batch(models.Model):
 
-        batch_name = models.TimeField(max_length=200, null=True, blank=True)
-        batch_startdate = models.DateField(null=True, blank=True)
-        batch_enddate = models.DateField(null=True, blank=True)
-        batch_timing = models.CharField(max_length=200,  null=True, blank=True)
-                
-        def __str__(self):
-            return self.batch_name
+class batch(models.Model):
+    batch_name = models.TimeField(max_length=200, null=True, blank=True)
+    batch_startdate = models.DateField(null=True, blank=True)
+    batch_enddate = models.DateField(null=True, blank=True)
+    batch_timing = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.batch_name
