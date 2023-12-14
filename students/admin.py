6a52586ee2b5bb6d369a13fbb0d633546ab60645
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-14 10:56:13 +0530

    Update student model to have permission and login

--- students/admin.py
+++ students/admin.py
@@ -1,43 +1,115 @@
 # students/admin.py
 
 from django.contrib import admin
-from .models import Student
 from import_export.admin import ImportExportModelAdmin
+
+from .models import Student
 
 
 class StudentAdmin(ImportExportModelAdmin):
-    list_display = ('first_name', 'last_name', 'course_to_enroll', 'whatsapp_no', 'last_education', 'ielts_taken_before', 'duolingo_taken_before', 'pte_taken_before',
-                    'toefl_taken_before','country_interested_in', 'remark' , 'email', 'interested_in_visa_counselling')
-    search_fields = ['first_name', 'last_name', 'email']
-    list_filter = ['course_to_enroll', 'gender', 'country_interested_in', 'state', 'city', 'ielts_taken_before', 'duolingo_taken_before',
-                   'pte_taken_before', 'toefl_taken_before', 'gre_taken_before', 'gmat_taken_before', 'interested_in_visa_counselling']
+    list_display = (
+        "first_name",
+        "last_name",
+        "course_to_enroll",
+        "whatsapp_no",
+        "last_education",
+        "ielts_taken_before",
+        "duolingo_taken_before",
+        "pte_taken_before",
+        "toefl_taken_before",
+        "country_interested_in",
+        "remark",
+        "email",
+        "interested_in_visa_counselling",
+    )
+    search_fields = ["first_name", "last_name", "email"]
+    list_filter = [
+        "course_to_enroll",
+        "gender",
+        "country_interested_in",
+        "state",
+        "city",
+        "ielts_taken_before",
+        "duolingo_taken_before",
+        "pte_taken_before",
+        "toefl_taken_before",
+        "gre_taken_before",
+        "gmat_taken_before",
+        "interested_in_visa_counselling",
+    ]
 
     # Jazzmin fieldsets
     fieldsets = (
-        ('Basic Info', {
-            'fields': ('first_name', 'middle_name', 'last_name', 'date_of_birth', 'gender',)
-        }),
-
-        ('Contact Info', {
-            'fields': ('country', 'state', 'city', 'whatsapp_no',)
-        }),
-
-        ('Other Info', {
-            'fields': (
-                'reference_by', 'country_interested_in', 'last_education', 'ielts_taken_before',
-                'duolingo_taken_before',
-                'pte_taken_before', 'toefl_taken_before', 'gre_taken_before', 'gmat_taken_before',
-                'interested_in_visa_counselling')
-        }),
-
-        ('Login Credential', {
-            'fields': ('email', 'password')
-        }),
-
-        ('Course To Enroll', {
-            'fields': ('course_to_enroll', 'remark',)
-        }),
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                    # "email",
+                    "date_of_birth",
+                    "gender",
+                    # "password",
+                )
+            },
+        ),
+        (
+            "Contact Info",
+            {
+                "fields": (
+                    "country",
+                    "state",
+                    "city",
+                    "whatsapp_no",
+                )
+            },
+        ),
+        (
+            "Other Info",
+            {
+                "fields": (
+                    "reference_by",
+                    "country_interested_in",
+                    "last_education",
+                    "ielts_taken_before",
+                    "duolingo_taken_before",
+                    "pte_taken_before",
+                    "toefl_taken_before",
+                    "gre_taken_before",
+                    "gmat_taken_before",
+                    "interested_in_visa_counselling",
+                )
+            },
+        ),
+        ("Login Credential", {"fields": ("email", "password")}),
+        (
+            "Course To Enroll",
+            {
+                "fields": (
+                    "course_to_enroll",
+                    "remark",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    # "groups",
+                    "user_permissions",
+                )
+            },
+        ),
     )
+
+    exclude = (
+        "is_superuser",
+        "is_staff",
+        "groups",
+    )
+
+    readonly_fields = ("last_login",)
 
 
 admin.site.register(Student, StudentAdmin)
--- students/models.py
+++ students/models.py
@@ -1,34 +1,64 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-#from package.models import Package  # Assuming the City, State, and Country are modeled in the Package app
-from Courses.models import Course  # For the Course To Enroll ForeignKey
-from master.models import City, State, Country  # For the City, State, and Country ForeignKey
+
+# from package.models import Package  # Assuming the City, State, and Country are modeled in the Package app
+from master.models import (
+    City,  # For the City, State, and Country ForeignKey
+    Country,
+    State,
+)
 from package.models import Package
 
-class Student(models.Model):
+
+class StudentManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class Student(AbstractBaseUser, PermissionsMixin):
     class Gender(models.TextChoices):
-        MALE = 'Male', 'Male'
-        FEMALE = 'Female', 'Female'
-        OTHER = 'Other', 'Other'
-        # ... you can add more choices as needed
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+        OTHER = "Other", "Other"
 
     class VisaCounsellingInterest(models.TextChoices):
-        YES = 'Yes', 'Yes'
-        NO = 'No', 'No'
-        # ... additional choices if needed
+        YES = "Yes", "Yes"
+        NO = "No", "No"
 
     # Section: Basic Info
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True, null=True)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.MALE)
-    country = models.ForeignKey(Country, related_name="student_country", on_delete=models.SET_NULL, null=True)
-    state = models.ForeignKey(State, related_name="student_state", on_delete=models.SET_NULL, null=True)
-    city = models.ForeignKey(City, related_name="student_city", on_delete=models.SET_NULL, null=True)
+    gender = models.CharField(
+        max_length=20, choices=Gender.choices, default=Gender.MALE
+    )
+    country = models.ForeignKey(
+        Country, related_name="student_country", on_delete=models.SET_NULL, null=True
+    )
+    state = models.ForeignKey(
+        State, related_name="student_state", on_delete=models.SET_NULL, null=True
+    )
+    city = models.ForeignKey(
+        City, related_name="student_city", on_delete=models.SET_NULL, null=True
+    )
     phone_no = models.CharField(max_length=15)
     whatsapp_no = models.CharField(max_length=15)
     reference_by = models.TextField()
-    country_interested_in = models.ForeignKey(Country, related_name="student_interest_country", on_delete=models.SET_NULL, null=True)
+    country_interested_in = models.ForeignKey(
+        Country,
+        related_name="student_interest_country",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     last_education = models.CharField(max_length=200)
     ielts_taken_before = models.BooleanField(default=False)
     duolingo_taken_before = models.BooleanField(default=False)
@@ -38,14 +68,24 @@ class Student(models.Model):
     gmat_taken_before = models.BooleanField(default=False)
     remark = models.TextField()
     biography = models.TextField()  # If you're using django-ckeditor or similar, this can be replaced with RichTextField
-    user_image = models.ImageField(upload_to='student_images/')
-    interested_in_visa_counselling = models.CharField(max_length=50, choices=VisaCounsellingInterest.choices, default=VisaCounsellingInterest.YES)
+    user_image = models.ImageField(upload_to="student_images/")
+    interested_in_visa_counselling = models.CharField(
+        max_length=50,
+        choices=VisaCounsellingInterest.choices,
+        default=VisaCounsellingInterest.YES,
+    )
     # Section: Login Credential
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)  # Storing password as CharField is not safe. Normally, you'd use Django's User model or a custom user model for handling authentication.
-
-    # Course To Enroll
+    password = models.CharField(max_length=100)
     course_to_enroll = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True)
+
+    groups = models.ManyToManyField("auth.Group", related_name="student_groups")
+    user_permissions = models.ManyToManyField(
+        "auth.Permission", related_name="student_user_permissions"
+    )
+    objects = StudentManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.first_name + " " + self.last_name
