63b541dcfff723a0fb70b4ec5115b810881c9bf3
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-18 17:05:13 +0530

    add course identifier in courses

--- Courses/models.py
+++ Courses/models.py
@@ -58,6 +58,9 @@ class Course(models.Model):
     )
 
     primary_instructor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    course_identifier = models.CharField(
+        max_length=200, null=True, blank=True, unique=True
+    )
 
     def __str__(self):
         return self.Course_Title
