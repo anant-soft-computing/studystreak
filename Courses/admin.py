813326cbd0919e968e8e5042bf4f5b3700c5cd2b
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-12-18 16:59:55 +0530

    add fieldsets in courseadmin

--- Courses/admin.py
+++ Courses/admin.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from master.models import CourseMaterial
+from master.models import AdditionalResource, CourseMaterial
 
 from .models import Course
 
@@ -13,9 +13,14 @@ class CourseMaterialInline(admin.TabularInline):
     extra = 1
 
 
+class AdditionalResourceInline(admin.TabularInline):
+    model = AdditionalResource
+    extra = 1
+
+
 @admin.register(Course)
 class CourseAdmin(ImportExportModelAdmin):
-    inlines = [CourseMaterialInline]
+    inlines = [CourseMaterialInline, AdditionalResourceInline]
     list_display = [
         "Course_Title",
         "Category",
@@ -23,7 +28,6 @@ class CourseAdmin(ImportExportModelAdmin):
         "Language",
         "EnrollmentStartDate",
         "EnrollmentEndDate",
-        "enrollment_count",
         "is_active",
         "Featured",
         "Support_Available",
@@ -41,7 +45,6 @@ class CourseAdmin(ImportExportModelAdmin):
         "Language",
         "EnrollmentStartDate",
         "EnrollmentEndDate",
-        "enrollment_count",
         "is_active",
         "faqs",
         "Featured",
@@ -59,80 +62,71 @@ class CourseAdmin(ImportExportModelAdmin):
         "Language",
         "EnrollmentStartDate",
         "EnrollmentEndDate",
-        "enrollment_count",
         "is_active",
         "faqs",
     ]
 
-    # Using fieldsets to organize the admin form into sections/tabs as per Jazzmin theme
-    # fieldsets = (
-    #     (
-    #         "Course Detail",
-    #         {
-    #             "fields": (
-    #                 "Course_Title",
-    #                 "Short_Description",
-    #                 "Description",
-    #                 "Category",
-    #                 "Level",
-    #                 "Language",
-    #                 "EnrollmentStartDate",
-    #                 "EnrollmentEndDate",
-    #                 "enrollment_count",
-    #                 "Featured",
-    #                 "Support_Available",
-    #                 "course_type",
-    #                 "course_delivery",
-    #             )
-    #         },
-    #     ),
-    #     (
-    #         "Requirements",
-    #         {
-    #             "fields": ("Requirements",),
-    #         },
-    #     ),
-    #     (
-    #         "Batch Timing",
-    #         {
-    #             "fields": ("Batch_Timing",),
-    #         },
-    #     ),
-    #     (
-    #         "Outcome",
-    #         {
-    #             "fields": ("Outcome",   'add_batch', 'add_package'),
-    #         },
-    #     ),
-    #     (
-    #         # "Course Price",
-    #         # {
-    #         #     "fields": ("Course_Price", "Discount_in_percent"),
-    #         # },
-    #     ),
-    #     (
-    #         "Course Media",
-    #         {
-    #             "fields": (
-    #                 "Course_Overview_Provider",
-    #                 "Course_Overview_URL",
-    #                 "Course_Thumbnail",
-    #             ),
-    #         },
-    #     ),
-    #     (
-    #         "Course SEO",
-    #         {
-    #             "fields": ("SEO_Meta_Keywords", "Meta_Description",),
-    #         },
-    #     ),
-    #     (
-    #         "Course Lessons",
-    #         {
-    #             "fields": ("Lessons",),
-    #         },
-    #     ),
-    # )
+    fieldsets = (
+        (
+            "Course Detail",
+            {
+                "fields": (
+                    "Course_Title",
+                    "Short_Description",
+                    "Description",
+                    "Category",
+                    "Level",
+                    "Language",
+                    "EnrollmentStartDate",
+                    "EnrollmentEndDate",
+                    "max_enrollments",
+                    "Featured",
+                    "Support_Available",
+                    "course_type",
+                    "course_delivery",
+                    "is_active",
+                    "faqs",
+                )
+            },
+        ),
+        (
+            "Requirements",
+            {
+                "fields": ("Requirements",),
+            },
+        ),
+        (
+            "Outcome",
+            {
+                "fields": ("Outcome",),
+            },
+        ),
+        (
+            "Course Media",
+            {
+                "fields": (
+                    "Course_Overview_Provider",
+                    "Course_Overview_URL",
+                    "Course_Thumbnail",
+                ),
+            },
+        ),
+        (
+            "Course SEO",
+            {
+                "fields": (
+                    "SEO_Meta_Keywords",
+                    "Meta_Description",
+                ),
+            },
+        ),
+        (
+            "Lesson",
+            {
+                "fields": ("lessons",),
+            },
+        ),
+    )
 
 
 # admin.site.register()
