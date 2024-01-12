7df18bad9e0f87185520d21b87ff1b3a327ca17c
MehulMachhi <machhimehul61@gmail.com>
2024-01-12 14:58:13 +0530

    minor changes

--- master/models.py
+++ master/models.py
@@ -113,7 +113,7 @@ class batch(models.Model):
     batch_start_timing = models.TimeField(null=True, blank=True)
     batch_end_timing = models.TimeField(null=True, blank=True)
     add_package = models.ForeignKey(
-        "package.package", on_delete=models.CASCADE, related_name="+"
+        "package.package", on_delete=models.CASCADE, related_name="+", null=True, blank=True
     )
 
     def __str__(self):
