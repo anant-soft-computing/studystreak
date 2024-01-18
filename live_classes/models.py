b5df9674c004d34c84e2ad84b2a90e17069a08a8
GaneshMadhavan99 <luckyganesh801@gmail.com>
2024-01-18 16:03:17 +0530

    live classes are committed

--- live_classes/models.py
+++ live_classes/models.py
@@ -1,24 +1,15 @@
-# live_classes/models.py
-
 from django.db import models
-
-from Courses.models import Course
-
+from master.models import batch
+from Courses.models import Course  # Assuming Course is another model you've defined
 
 class LiveClass(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    package = models.ForeignKey(
-        "package.Package", on_delete=models.CASCADE, null=True, blank=True
-    )
-    batch = models.ForeignKey(
-        "master.batch", on_delete=models.CASCADE, null=True, blank=True
-    )
+    batch = models.ForeignKey(batch, on_delete=models.CASCADE, null=True, blank=True)
     meeting_title = models.CharField(max_length=255)
-    meeting_description = models.TextField()  # If you're using django-ckeditor or similar, this can be replaced with RichTextField
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    zoom_meeting_id = models.CharField(max_length=100)
-    zoom_meeting_password = models.CharField(max_length=100)
+    meeting_description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    zoom_meeting_id = models.CharField(max_length=100, blank=True)
+    zoom_meeting_password = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.meeting_title
