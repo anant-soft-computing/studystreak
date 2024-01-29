1685a91a22146e67315bd0ac81012371683cfc5c
MehulMachhi <machhimehul61@gmail.com>
2024-01-29 19:50:50 +0530

    courseretupddelview some changes

--- coursedetail/serializers.py
+++ coursedetail/serializers.py
@@ -15,7 +15,7 @@ class LessonListSerializers(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
-        depth = 2
+        depth = 4
 
 class LessonDetailSerializer(serializers.ModelSerializer):
     attachment_lession_count = serializers.SerializerMethodField()
@@ -32,7 +32,7 @@ class LessonDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
-        depth = 3
+        depth = 4
 
     def get_attachment_lession(self, lesson):
         return LessonAttachment.objects.filter(lesson=lesson)
@@ -72,6 +72,7 @@ class LessionRetUpdDelView(generics.RetrieveUpdateDestroyAPIView):
     class Meta:
         model = Lesson
         fields = '__all__'
+        depth = 2
 
 
 # class QuizOptionListSerializers(serializers.ModelSerializer):
