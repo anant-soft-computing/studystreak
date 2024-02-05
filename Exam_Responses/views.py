9581e1e00e3b006bed47cc9ab6cf47c65f680a66
MehulMachhi <machhimehul61@gmail.com>
2024-02-05 12:58:06 +0530

    studentanswerlistview minor change

--- Exam_Responses/views.py
+++ Exam_Responses/views.py
@@ -4,5 +4,6 @@ from rest_framework import generics
 from .models import StudentAnswer
 
 class StudentAnswerListView(generics.ListCreateAPIView):
+    # queryset = StudentAnswer
     queryset = StudentAnswer.objects.all()
     serializer_class = StudentAnswerSerializers
