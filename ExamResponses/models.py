a2cab895d53e3fb2d33669ec44d6d65811b6eefc
AmitPatel <amit.patel@vayana.com>
2024-02-08 17:53:59 +0530

    make changes in ExamResponse app

--- ExamResponses/models.py
+++ ExamResponses/models.py
@@ -1,19 +1,18 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 # Create your models here.
 from exam.models import Exam
 
 
-
 class Studentanswer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    exam = models.ForeignKey(Exam, related_name="exam", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
 
 class Student_answer(models.Model):
-    student_exam = models.ForeignKey(Studentanswer, related_name="student_exam", on_delete=models.CASCADE)
-    question_number = (
-        models.IntegerField()
-    ) 
+    student_exam = models.ForeignKey(Studentanswer, on_delete=models.CASCADE)
+    question_number = models.IntegerField()
     answer_text = models.TextField()
 
     def __str__(self):
